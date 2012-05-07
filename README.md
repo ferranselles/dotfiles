bf5ed67cfa0dad4ed13490b276f695c599803f6e
ferran.selles@gmail.com <ferran.selles@gmail.com>
2012-05-07 15:47:12 +0200

    Pushing tmux config for now using tmuxified

--- README.md
+++ README.md
@@ -10,6 +10,11 @@ Inspired by:
 - https://github.com/spf13/spf13-vim/blob/3.0/.vimrc
 - https://github.com/skwp/dotfiles/blob/master/vimrc
 
+##TMUX
+Now using: tmuxified
+
+- https://github.com/zaiste/tmuxified
+
+
 #TODO
 - Add zsh config
-- Add tmux config
--- install.sh
+++ install.sh
@@ -5,3 +5,10 @@ mkdir -p ~/.vim/bundle
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 cp vim/vimrc ~/.vimrc
 vim +BundleInstall +qall
+
+#SETUP tmux
+su - root -c "easy_install psutil"
+git clone git://github.com/zaiste/tmuxified.git ~/.tmuxified
+ln -sfn ~/.tmuxified ~/.tmux
+ln -sfn ~/.tmuxified/tmux.conf ~/.tmux.conf
+ln -sfn ~/.tmuxified/scripts/basic-cpu-and-memory.tmux ~/bin/basic-cpu-and-memory.tmux
