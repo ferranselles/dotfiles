7809cbb28439cd3591517047edd34487f5cb62a4
Ferran Selles <ferran.selles@gmail.com>
2012-09-25 11:20:18 +0200

    Updated install.sh

--- install.sh
+++ install.sh
@@ -7,8 +7,13 @@ cp vim/vimrc ~/.vimrc
 vim +BundleInstall +qall
 
 #SETUP tmux
+#if centos62 we need to install easy_install for python
+#rpm -Uhv http://ftp.rediris.es/mirror/fedora-epel/6/i386/epel-release-6-5.noarch.rpm
+#yum install python-devel python-pip
+
 su - root -c "easy_install psutil"
 git clone git://github.com/zaiste/tmuxified.git ~/.tmuxified
 ln -sfn ~/.tmuxified ~/.tmux
 ln -sfn ~/.tmuxified/tmux.conf ~/.tmux.conf
+mkdir -p ~/bin
 ln -sfn ~/.tmuxified/scripts/basic-cpu-and-memory.tmux ~/bin/basic-cpu-and-memory.tmux
